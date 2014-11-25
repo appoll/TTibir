dd09e0fb774f4f5cd5262e67014748de3e1bcb59
Paul Anton <paul.antton@gmail.com>
2014-11-25 17:10:15 +0200

    Set up parse account and app.

---
+++ app/src/main/java/antton/paul/ttibir/TTibirApplication.java
@@ -0,0 +1,23 @@
+package antton.paul.ttibir;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Paul's on 25-Nov-14.
+ */
+public class TTibirApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "wcVmVWTEHrSCuZ4e08AfLpuKkTpp0vAKVkthI1Lb", "uZvQa4jHNUMldnHVP7kPiRzK7Ldf87kYPPskpjZk");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+
+}
