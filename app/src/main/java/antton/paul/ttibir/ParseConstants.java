0efac1b55510561ba90321e49e06d8c11508ce77
Paul Anton <paul.antton@gmail.com>
2014-11-25 22:31:57 +0200

    Built query to get all users from parse. Got the desired results. Happy yep yep.

---
+++ app/src/main/java/antton/paul/ttibir/ParseConstants.java
@@ -0,0 +1,12 @@
+package antton.paul.ttibir;
+
+/**
+ * Created by Paul's on 25-Nov-14.
+ */
+public final class ParseConstants {
+    // Class name
+
+
+    // Field names
+    public static final String KEY_USERNAME = "username";
+}
