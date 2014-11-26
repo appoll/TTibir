d4364151c66cfc21d3654afb9eed497dda312e72
Paul Anton <paul.antton@gmail.com>
2014-11-26 21:49:28 +0200

    View video messages

--- app/src/main/java/antton/paul/ttibir/ViewImageActivity.java
+++ app/src/main/java/antton/paul/ttibir/ViewImageActivity.java
@@ -27,20 +27,4 @@ public class ViewImageActivity extends Activity {
         Picasso.with(this).load(imageUri.toString()).into(imageView);
     }
 
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
