d5c93d1deb4e7ed3c4843b419373226e9aeee11b
Paul Anton <paul.antton@gmail.com>
2014-11-27 00:40:45 +0200

    Hide action bar for login and signup screen.

--- app/src/main/java/antton/paul/ttibir/LoginActivity.java
+++ app/src/main/java/antton/paul/ttibir/LoginActivity.java
@@ -1,5 +1,6 @@
 package antton.paul.ttibir;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -32,6 +33,9 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
 
         mSignUpTextView = (TextView) findViewById(R.id.signUpText);
         mSignUpTextView.setOnClickListener(new View.OnClickListener() {
