f9d2f9c164fb598cefe5bcbf0670871bbb2a9219
Paul Anton <paul.antton@gmail.com>
2014-11-27 22:46:31 +0200

    Retrieve gravatar image

--- app/src/main/java/antton/paul/ttibir/adapters/UserAdapter.java
+++ app/src/main/java/antton/paul/ttibir/adapters/UserAdapter.java
@@ -11,11 +11,13 @@ import android.widget.TextView;
 
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.squareup.picasso.Picasso;
 
 import java.util.Date;
 import java.util.List;
 
 import antton.paul.ttibir.R;
+import antton.paul.ttibir.utils.MD5Util;
 import antton.paul.ttibir.utils.ParseConstants;
 
 /**
@@ -44,7 +46,7 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
         if (convertView == null) {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.user_item, null);
             holder = new ViewHolder();
-         //   holder.iconImageView = (ImageView) convertView.findViewById(R.id.messageIcon);
+           holder.userImageView = (ImageView) convertView.findViewById(R.id.userImageView);
             holder.nameLabel = (TextView) convertView.findViewById(R.id.nameLabel);
 
             convertView.setTag(holder);
@@ -55,6 +57,21 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
         }
 
         ParseUser user= mUsers.get(position);
+        String email = user.getEmail().toLowerCase();
+
+        if (email.equals("")){
+            holder.userImageView.setImageResource(R.drawable.avatar_empty);
+        }
+        else
+        {
+            String hash = MD5Util.md5Hex(email);
+            String gravatarUrl = "http://www.gravatar.com/avatar/"+hash+"?s=204&d=404";
+            Picasso.with(mContext)
+                    .load(gravatarUrl)
+                    .placeholder(R.drawable.avatar_empty)
+                    .into(holder.userImageView);
+        }
+
         Date createdAt = user.getCreatedAt();
         long now = new Date().getTime();
         String convertedDate = DateUtils.getRelativeTimeSpanString(
@@ -78,7 +95,7 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
     }
 
     private static class ViewHolder {
-       // ImageView iconImageView;
+        ImageView userImageView;
         TextView nameLabel;
 
     }
